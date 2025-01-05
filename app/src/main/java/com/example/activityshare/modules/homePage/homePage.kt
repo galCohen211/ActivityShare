484b936d819f6c0833c1f540574dd5b4f0b683b4
Gal Cohen <galcohen211@gmail.com>
2025-01-05 13:05:55 +0200

    Control bottom bar visibility for specific fragments (Login/Register)

--- app/src/main/java/com/example/activityshare/modules/homePage/homePage.kt
+++ app/src/main/java/com/example/activityshare/modules/homePage/homePage.kt
@@ -5,10 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
 import com.example.activityshare.R
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class homePage : Fragment() {
 
@@ -20,25 +17,7 @@ class homePage : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home_page, container, false)
-
-        // bottom bar navigation
-        val bottomNavigationView = view.findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        NavigationUI.setupWithNavController(bottomNavigationView, navController)
-
-        bottomNavigationView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.item_profile -> {
-                    navController.navigate(R.id.action_homePage_to_profile)
-                    true
-                }
-                else -> false
-            }
-        }
-
         return view
     }
 }
